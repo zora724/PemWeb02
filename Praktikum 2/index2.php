af12bcaae2b0b0be705708f23d4c34b6b2aec373
zora724 <114472522+zora724@users.noreply.github.com>
2023-03-05 20:15:25 +0700

    Delete Praktikum 2 directory

--- Praktikum 2/index2.php
+++
@@ -1,9 +0,0 @@
-<?php
-   $customer = $_POST['customer'];
-   $produk = $_POST['pilihproduk'];
-   $jumlah = $_POST['jumlah'];
-
-   echo 'Customer : ' . $customer;
-   echo '<br> Produk Yang Dipilih : ' . $produk;
-   echo '<br> Jumlah Item : ' . $jumlah
- ?>
